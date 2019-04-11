0fb6498228a3d7c2b73f4e230cea154acebc2deb
Joshua Warren <Jw130063@students.davenport.k12.ia.us>
2019-04-11 13:18:18 -0500

    update

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args)
+    {
+  int num1;
+  int num2;
+  int num3;
+
+  num1= 10;
+  num2 = 35;
+  num3 = num1 + num2;
+  System.out.println(num3);
+  num3 = num1 * num2;
+  System.out.println(num3);
+  num3 = num2 % num1;
+  System.out.println(num3);
+
+  Scanner input = new Scanner(System.in);
+  System.out.println("Please enter a whole number>>>>");
+  num2 = input.nextInt();
+  System.out.println("Please enter another whole number>>>>");
+
+   num1=
+
+
+
+
+    }
+}
